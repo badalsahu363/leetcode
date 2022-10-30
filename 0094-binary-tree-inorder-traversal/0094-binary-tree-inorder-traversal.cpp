c56312e6e7978ef88ceecb5bbc541b37494d8e1e
Aurobind sahu <62556848+badalsahu363@users.noreply.github.com>
2022-10-30 16:10:31 +0530

    Time: 0 ms (100.00%), Space: 8.4 MB (75.10%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.cpp
@@ -15,18 +15,22 @@ public:
         vector<int>inorder;
         TreeNode* curr = root;
         while(curr != NULL){
+            //case 1: if curr ->left is null then store and go to right
             if(curr -> left == NULL){
                 inorder.push_back(curr->val);
                 curr = curr->right;
             }else{
                 TreeNode* prev = curr->left;
+                //go to the right most part
                 while(prev->right && prev->right != curr){
                     prev = prev->right;
                 }
+                //case 2: first check and then traverse: if prev->right is null then connect thread and then traverse left 
                 if(prev->right == NULL){
                     prev->right = curr;
                     curr =  curr->left;
                 }else{
+                    //case 3: if prev->right is not null then cut the thread and store and go to the right part
                     prev->right = NULL;
                     inorder.push_back(curr->val);
                     curr = curr->right;
