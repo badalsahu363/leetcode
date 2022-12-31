0a402d735775f3bafcf8fe0bfd25718c03c281d3
Aurobind sahu <62556848+badalsahu363@users.noreply.github.com>
2022-12-31 19:07:50 +0530

    Time: 665 ms (5.01%), Space: 396.4 MB (5.00%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.cpp
+++ 0200-number-of-islands/0200-number-of-islands.cpp
@@ -1,73 +1,43 @@
-class Solution
-{
-    public:
-        void BFS(vector<vector < char>> &grid, int row, int col, vector< vector< int>> &visited, int del_row[], int del_col[])
-        {
-            visited[row][col] = 1;
-            int n = grid.size();
-            int m = grid[0].size();
-            queue<pair<int, int>> q;
-            q.push({ row,
-                col });
-            while (!q.empty())
-            {
-                int row = q.front().first;
-                int col = q.front().second;
-                q.pop();
-
-               	//checking all  8 direction (according to the video)
-               	//                 for (int delcol = -1; delcol <= 1; delcol++)
-               	//                 {
-               	//                     for (int delrow = -1; delrow <= 1; delrow++)
-               	//                     {
-               	//                         int nrow = delrow + row;
-               	//                         int ncol = delcol + col;
-               	//                         if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && grid[nrow][ncol] == '1' && visited[nrow][ncol] == 0)
-               	//                         {
-               	//                             visited[nrow][ncol] = 1;
-               	//                             q.push({ nrow,
-               	//                                 ncol });
-               	//                         }
-               	//                     }
-               	//                 }
-
-               	// But here we have to check only 4 direction 
-                for (int i = 0; i < 4; i++)
-                {
-
-                    int nrow = del_row[i] + row;
-                    int ncol = del_col[i] + col;
-                    if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && grid[nrow][ncol] == '1' && visited[nrow][ncol] == 0)
-                    {
-                        visited[nrow][ncol] = 1;
-                        q.push({ nrow,
-                            ncol });
+class Solution {
+public:
+    void bfs(int row, int col, vector<vector<int>>& vis, vector<vector<char>> grid) {
+        vis[row][col] = 1;
+        queue<pair<int, int> > q;
+        q.push({row, col});
+        
+        while(!q.empty()) {
+            int row=q.front().first;
+            int col=q.front().second;
+            q.pop();
+            
+            //for neighbouring lands.
+            for(int drow=-1; drow<=1; drow++) {
+                for(int dcol=-1; dcol<=1; dcol++){
+                    if(drow==-1&&dcol==0||drow==0&&dcol==1||drow==1&&dcol==0||drow==0&&dcol==-1){
+                        int nrow=row+drow;
+                        int ncol=col+dcol;
+                        if(nrow>=0 && nrow<grid.size() && ncol>=0 && ncol<grid[0].size() 
+                           && grid[nrow][ncol]=='1' && !vis[nrow][ncol]) {
+                            vis[nrow][ncol]=1;
+                            q.push({nrow, ncol});
+                        }
                     }
                 }
             }
         }
-
-    int numIslands(vector<vector < char>> &grid)
-    {
-
-        int n = grid.size();
-        int m = grid[0].size();
-        vector<vector < int>> visited(n, vector<int> (m, 0));
-        int count = 0;
-       	// for four directions 
-        int del_row[] = { -1,0,1,0};
-        int del_col[] = { 0,1,0,-1};
-        for (int i = 0; i < n; i++)
-        {
-            for (int j = 0; j < m; j++)
-            {
-
-                if (visited[i][j] == 0 && grid[i][j] == '1')
-                {
-                    int row = i;
-                    int col = j;
+    }
+    
+    int numIslands(vector<vector<char>>& grid) {
+        int n=grid.size();
+        int m=grid[0].size();
+        vector<vector<int> > vis(n, vector<int>(m,0));
+        int count=0;
+        
+        for(int row=0; row<n; row++) {
+            for(int col=0; col<m; col++) {
+                if(!vis[row][col] && grid[row][col] == '1') {
                     count++;
-                    BFS(grid, row, col, visited, del_row, del_col);
+                    bfs(row, col, vis, grid);
                 }
             }
         }
