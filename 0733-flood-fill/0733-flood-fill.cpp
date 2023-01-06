8a899af0f3f3713ed1652c627bee7ccc899ff214
Aurobind sahu <62556848+badalsahu363@users.noreply.github.com>
2023-01-06 21:06:24 +0530

    Time: 7 ms (94.05%), Space: 13.9 MB (91.79%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,34 +1,26 @@
 class Solution {
 private:
-    void dfs(int row, int col, vector<vector<int>>&ans,
-     vector<vector<int>>& image, int newColor, int delRow[], int delCol[],
-     int iniColor) {
-        // color with new color
-        ans[row][col] = newColor; 
-        int n = image.size();
-        int m = image[0].size(); 
-        // there are exactly 4 neighbours
-        for(int i = 0;i<4;i++) {
-            int nrow = row + delRow[i]; 
-            int ncol = col + delCol[i]; 
-            // check for valid coordinate 
-            // then check for same initial color and unvisited pixel
-            if(nrow>=0 && nrow<n && ncol>=0 && ncol < m && 
-            image[nrow][ncol] == iniColor && ans[nrow][ncol] != newColor) {
-                dfs(nrow, ncol, ans, image, newColor, delRow, delCol, iniColor); 
+    void dfs(int row,int col,vector<vector<int>>& image,vector<vector<int>>&ans,
+             int n,int m,int color,int delRow[],int delCol[],int init){
+        ans[row][col] = color;
+        for(int i = 0;i<4;i++){
+            int nrow =  row + delRow[i];
+            int ncol =  col + delCol[i];
+            if(nrow < n && nrow >= 0  && ncol < m && ncol >= 0 
+               && image[nrow][ncol] == init && ans[nrow][ncol] != color){
+                dfs(nrow,ncol,image,ans,n,m,color,delRow,delCol,init);
             }
         }
     }
 public:
-    vector<vector<int>> floodFill(vector<vector<int>>& image, 
-    int sr, int sc, int newColor) {
-        // get initial color
-        int iniColor = image[sr][sc]; 
-        vector<vector<int>> ans = image; 
-        // delta row and delta column for neighbours
-        int delRow[] = {-1, 0, +1, 0};
-        int delCol[] = {0, +1, 0, -1}; 
-        dfs(sr, sc, ans, image, newColor, delRow, delCol, iniColor); 
-        return ans; 
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        int init = image[sr][sc];
+        int n = image.size();
+        int m = image[0].size();
+        int delRow[] = { -1,0,+1,0};
+        int delCol[] = {0,+1,0,-1};
+        vector<vector<int>>ans = image;
+        dfs(sr,sc,image,ans,n,m,color,delRow,delCol,init);
+        return ans;
     }
 };
