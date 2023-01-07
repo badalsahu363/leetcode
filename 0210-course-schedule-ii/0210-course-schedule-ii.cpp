83fb8d2a6adfef45edcc56ab78093f69de40bdea
Aurobind sahu <62556848+badalsahu363@users.noreply.github.com>
2023-01-07 12:21:28 +0530

    Time: 11 ms (99.90%), Space: 13.2 MB (94.02%) - LeetHub

---
+++ 0210-course-schedule-ii/0210-course-schedule-ii.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> findOrder(int n, vector<vector<int>>& prerequisites) {
+         vector<int>adj[n];
+        for(int i =0;i<prerequisites.size();i++){
+            adj[prerequisites[i][1]].push_back(prerequisites[i][0]);
+        }
+        vector<int>topo;
+        vector<int>indegree(n,0);
+        queue<int>q;
+        for(int i=0;i<n;i++){
+            for(auto it : adj[i]){
+                indegree[it]++;
+            }
+        }
+        for(int i =0;i<n;i++){
+            if(indegree[i] == 0){
+                q.push(i);
+            }
+        }
+        while(!q.empty()){
+            int node = q.front();
+            q.pop();
+            topo.push_back(node);
+            for(auto it : adj[node]){
+                indegree[it]--;
+                if(indegree[it] == 0) q.push(it);
+            }
+        }
+        if(topo.size() == n) return topo;
+        return {};
+    }
+};
