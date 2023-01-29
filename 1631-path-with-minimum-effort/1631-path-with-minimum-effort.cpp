502dde24b598809f30eb1afd2cc6875e2d4a10f3
Aurobind sahu <62556848+badalsahu363@users.noreply.github.com>
2023-01-29 11:34:52 +0530

    Time: 144 ms (94.01%), Space: 20 MB (61.98%) - LeetHub

---
+++ 1631-path-with-minimum-effort/1631-path-with-minimum-effort.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int minimumEffortPath(vector<vector<int>>& heights) {
+         priority_queue<pair<int,pair<int,int>>, vector<pair<int,pair<int,int>>>,
+        greater<pair<int,pair<int,int>>>>pq;
+        int n = heights.size();
+        int m = heights[0].size();
+        vector<vector<int>>dis(n,vector<int>(m,1e9));
+        dis[0][0] = 0;
+        pq.push({0,{0,0}});
+         int delrow[] = {-1,0,+1,0};
+         int delcol[] = {0,+1,0,-1};
+        while(!pq.empty()){
+            auto it = pq.top();
+            pq.pop();
+            int diff = it.first;
+            int row = it.second.first;
+            int col = it.second.second;
+            
+            if(row == n-1 && col == m-1){
+                return diff;
+            }
+            
+            for(int i =0;i<4;i++){
+                int nrow = row + delrow[i];  
+                int ncol = col + delcol[i];
+                
+                if(nrow >= 0 && nrow < n && ncol >=0 && ncol < m ){
+                    int newEffort = max(abs(heights[row][col] - heights[nrow][ncol]),diff);
+                    if(newEffort  < dis[nrow][ncol]){
+                        dis[nrow][ncol] = newEffort;
+                        pq.push({newEffort,{nrow,ncol}});
+                    }
+                }
+            }
+        }
+        return 0;
+    }
+};
