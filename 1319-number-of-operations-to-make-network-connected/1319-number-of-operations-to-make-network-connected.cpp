22aaeeb70fc7a06f44f26315645889acdc0eca4f
Aurobind sahu <62556848+badalsahu363@users.noreply.github.com>
2023-02-06 21:58:53 +0530

    Time: 126 ms (83.39%), Space: 42.7 MB (59.67%) - LeetHub

---
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -0,0 +1,81 @@
+class DisjointSet {
+public: 
+    vector<int> rank, parent, size; 
+    DisjointSet(int n) {
+        rank.resize(n+1, 0); 
+        parent.resize(n+1);
+        size.resize(n+1); 
+        for(int i = 0;i<=n;i++) {
+            parent[i] = i; 
+            size[i] = 1; 
+        }
+    }
+
+    int findUPar(int node) {
+        if(node == parent[node])
+            return node; 
+        return parent[node] = findUPar(parent[node]); 
+    }
+
+    void unionByRank(int u, int v) {
+        int ulp_u = findUPar(u); 
+        int ulp_v = findUPar(v); 
+        if(ulp_u == ulp_v) return; 
+        if(rank[ulp_u] < rank[ulp_v]) {
+            parent[ulp_u] = ulp_v; 
+        }
+        else if(rank[ulp_v] < rank[ulp_u]) {
+            parent[ulp_v] = ulp_u; 
+        }
+        else {
+            parent[ulp_v] = ulp_u; 
+            rank[ulp_u]++; 
+        }
+    }
+
+    void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u); 
+        int ulp_v = findUPar(v); 
+        if(ulp_u == ulp_v) return; 
+        if(size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v; 
+            size[ulp_v] += size[ulp_u]; 
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v]; 
+        }
+    }
+}; 
+class Solution {
+public:
+    int makeConnected(int n, vector<vector<int>>& edge) {
+        DisjointSet ds(n);
+       int extraEdges = 0;
+       for(auto it : edge){
+           int  u = it[0];
+           int v = it[1];
+           
+           if(ds.findUPar(u) == ds.findUPar(v)){
+               extraEdges++;
+           }else{
+               ds.unionBySize(u,v);
+           }
+       }
+       //count the components
+       int CountComp = 0;
+       for(int  i=0;i<n;i++){
+           if(ds.parent[i] == i){
+               CountComp++;
+           }
+       }
+       
+       
+       int ans  =  CountComp - 1;
+       if(extraEdges >= ans){
+           return ans;
+       }else{
+           return -1;
+       }
+    }
+};
