0bfcc05729a628b23ec86908b291b27d43e742fd
Aurobind sahu <62556848+badalsahu363@users.noreply.github.com>
2023-02-08 00:13:03 +0530

    Time: 475 ms (80.99%), Space: 111.3 MB (62.66%) - LeetHub

--- 0827-making-a-large-island/0827-making-a-large-island.cpp
+++ 0827-making-a-large-island/0827-making-a-large-island.cpp
@@ -54,53 +54,53 @@ private:
         return row>=0 && row < n && col >=0 && col <n;
     }
 public:
-    int largestIsland(vector<vector<int>>& grid) {
-       int n = grid.size();
-        DisjointSet ds(n * n);
-        // step - 1
-        for (int row = 0; row < n ; row++) {
-            for (int col = 0; col < n ; col++) {
-                if (grid[row][col] == 0) continue;
-                int dr[] = { -1, 0, 1, 0};
-                int dc[] = {0, -1, 0, 1};
-                for (int ind = 0; ind < 4; ind++) {
-                    int newr = row + dr[ind];
-                    int newc = col + dc[ind];
-                    if (isValid(newr, newc, n) && grid[newr][newc] == 1) {
+    int largestIsland(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        DisjointSet ds(n*n);
+        //step 1 : iterate over the matrix and find 1  and then traverse i n 4 directions
+        for(int row =0;row < n;row++){
+            for(int col =0;col<n;col++){
+                if(matrix[row][col] == 0) continue;
+                int delRow[] = {-1,0,1,0};
+                int delCol[] = {0,-1,0,1};
+                for(int ind = 0;ind <4;ind++){
+                    int nrow = row  + delRow[ind];
+                    int ncol = col + delCol[ind];
+                    if(isValid(nrow,ncol,n) && matrix[nrow][ncol] == 1){
                         int nodeNo = row * n + col;
-                        int adjNodeNo = newr * n + newc;
-                        ds.unionBySize(nodeNo, adjNodeNo);
+                        int adjNo = nrow * n + ncol;
+                        ds.unionBySize(nodeNo,adjNo);
                     }
                 }
             }
         }
-        // step 2
-        int mx = 0;
-        for (int row = 0; row < n; row++) {
-            for (int col = 0; col < n; col++) {
-                if (grid[row][col] == 1) continue;
-                int dr[] = { -1, 0, 1, 0};
-                int dc[] = {0, -1, 0, 1};
-                set<int> components;
-                for (int ind = 0; ind < 4; ind++) {
-                    int newr = row + dr[ind];
-                    int newc = col + dc[ind];
-                    if (isValid(newr, newc, n)) {
-                        if (grid[newr][newc] == 1) {
-                            components.insert(ds.findUPar(newr * n + newc));
+        int ans = 0;
+        for(int row =0;row <n;row++){
+             for(int col =0;col<n;col++){
+                if(matrix[row][col] == 1) continue;
+                int delRow[] = {-1,0,1,0};
+                int delCol[] = {0,-1,0,1};
+                 set<int>storeUparent;
+                for(int ind = 0;ind <4;ind++){
+                    int nrow = row  + delRow[ind];
+                    int ncol = col + delCol[ind];
+                    if(isValid(nrow,ncol,n)){
+                        if(matrix[nrow][ncol] == 1){
+                            storeUparent.insert(ds.findUPar(nrow*n+ncol));
                         }
                     }
-                }
-                int sizeTotal = 0;
-                for (auto it : components) {
-                    sizeTotal += ds.size[it];
-                }
-                mx = max(mx, sizeTotal + 1);
-            }
+                } 
+                 int totalSum =0;
+                for(auto it : storeUparent){
+                    totalSum += ds.size[it];
+                } 
+                 ans = max(totalSum+1,ans);
+             }
         }
-        for (int cellNo = 0; cellNo < n * n; cellNo++) {
-            mx = max(mx, ds.size[ds.findUPar(cellNo)]);
+        
+        for(int cell = 0;cell < n*n;cell++){
+            ans  = max(ds.size[ds.findUPar(cell)],ans);
         }
-        return mx;
+        return ans;
     }
 };
