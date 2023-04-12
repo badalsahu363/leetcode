df28cd755b1b0c598dca8cd68c4956f3955f8704
Aurobind sahu <62556848+badalsahu363@users.noreply.github.com>
2023-04-12 19:35:03 +0530

    Time: 7 ms (66.32%), Space: 13.7 MB (83.65%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.cpp
@@ -12,16 +12,31 @@ public:
 //         return dp[n];
 //     }
     
+//     int minCostClimbingStairs(vector<int>& cost) {
+//         int n = cost.size();
+//         vector<int>dp(n+1);
+//         //step1 : base case
+//         dp[0] = cost[0];
+//         dp[1] = cost[1];
+        
+//         for(int i =2;i<n;i++){
+//             dp[i] = cost[i] + min(dp[i-1] , dp[i-2]);
+//         }
+//         return min(dp[n-1],dp[n-2]);
+//     }
+    
     int minCostClimbingStairs(vector<int>& cost) {
         int n = cost.size();
-        vector<int>dp(n+1);
         //step1 : base case
-        dp[0] = cost[0];
-        dp[1] = cost[1];
+       int prev2 = cost[0];
+       int prev1 = cost[1];
         
         for(int i =2;i<n;i++){
-            dp[i] = cost[i] + min(dp[i-1] , dp[i-2]);
+          int curr = cost[i] + min(prev1 , prev2);
+        
+          prev2 = prev1;
+          prev1 = curr;
         }
-        return min(dp[n-1],dp[n-2]);
+        return min(prev1,prev2);
     }
 };
