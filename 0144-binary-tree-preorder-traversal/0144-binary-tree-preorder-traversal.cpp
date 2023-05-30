19fe9e17778b82a119630f93f49a241807c5daf0
Aurobind sahu <62556848+badalsahu363@users.noreply.github.com>
2023-05-30 18:49:54 +0530

    Time: 0 ms (100.00%), Space: 8.6 MB (15.37%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -1,43 +1,28 @@
 /**
- *Definition for a binary tree node.
- *struct TreeNode {
- *    int val;
- *    TreeNode * left;
- *    TreeNode * right;
- *    TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- *};
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
  */
-class Solution
-{
-    public:
-        vector<int> preorderTraversal(TreeNode *root) {
-             vector<int>preorder;
-            TreeNode* curr = root;
-        while(curr != NULL){
-            //case 1: if curr ->left is null then store and go to right
-            if(curr -> left == NULL){
-                preorder.push_back(curr->val);
-                curr = curr->right;
-            }else{
-                TreeNode* prev = curr->left;
-                //go to the right most part
-                while(prev->right && prev->right != curr){
-                    prev = prev->right;
-                }
-                //case 2: first check and then traverse: if prev->right is null then connect thread and then traverse left 
-                if(prev->right == NULL){
-                    prev->right = curr;
-                    preorder.push_back(curr->val);
-                    curr =  curr->left;
-                }else{
-                    //case 3: if prev->right is not null then cut the thread and and go to the right part
-                    prev->right = NULL;
-                    curr = curr->right;
-                }
+class Solution {
+public:
+    vector<int> preorderTraversal(TreeNode* root) {
+         vector<int>preorder;
+            if(root == NULL) return preorder; 
+            stack<TreeNode *>s;
+            s.push(root);
+            while(!s.empty()){
+                 root = s.top();
+                s.pop();
+                preorder.push_back(root->val);
+                if(root->right != NULL)  s.push(root->right);
+                if(root->left != NULL)  s.push(root->left);
             }
-        }
             return preorder;
-        }
+    }
 };
