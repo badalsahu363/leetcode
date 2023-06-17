07c0428710204a44a7813c46fe4352ce5485f110
Aurobind sahu <62556848+badalsahu363@users.noreply.github.com>
2023-06-17 13:12:20 +0530

    Time: 783 ms (42.06%), Space: 207.1 MB (36.88%) - LeetHub

--- 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 0123-best-time-to-buy-and-sell-stock-iii/0123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,32 +1,33 @@
 class Solution {
 public:
-    int getAns(vector<int>& Arr, int n, int ind, int buy, int cap, vector<vector<vector<int>>>& dp ){
-
-    if(ind==n || cap==0) return 0; //base case
-    
-    if(dp[ind][buy][cap]!=-1)
-        return dp[ind][buy][cap];
-        
-    int profit;
-    
-    if(buy==0){// We can buy the stock
-        profit = max(0+getAns(Arr,n,ind+1,0,cap,dp), 
-                    -Arr[ind] + getAns(Arr,n,ind+1,1,cap,dp));
-    }
-    
-    if(buy==1){// We can sell the stock
-        profit = max(0+getAns(Arr,n,ind+1,1,cap,dp),
-                    Arr[ind] + getAns(Arr,n,ind+1,0,cap-1,dp));
-    }
-    
-    return dp[ind][buy][cap] = profit;
-}
-    int maxProfit(vector<int>& prices) {
-        int n = prices.size();
-         vector<vector<vector<int>>> dp(n,
+    int maxProfit(vector<int>& Arr){
+        int n = Arr.size();
+    // Creating a 3d - dp of size [n+1][2][3] initialized to 0
+    vector<vector<vector<int>>> dp(n+1,
                                     vector<vector<int>> 
-                                            (2,vector<int>(3,-1)));
+                                            (2,vector<int>(3,0)));
+                                            
+    // As dp array is intialized to 0, we have already covered the base case
     
-    return getAns(prices,n,0,0,2,dp);
+    for(int ind = n-1; ind>=0; ind--){
+        for(int buy = 0; buy<=1; buy++){
+            for(int cap=1; cap<=2; cap++){
+                
+                if(buy==0){// We can buy the stock
+                    dp[ind][buy][cap] = max(0+dp[ind+1][0][cap], 
+                                -Arr[ind] + dp[ind+1][1][cap]);
+                 }
+    
+                if(buy==1){// We can sell the stock
+                    dp[ind][buy][cap] = max(0+dp[ind+1][1][cap],
+                                Arr[ind] + dp[ind+1][0][cap-1]);
+                }
+            }
+        }
     }
+    
+    
+    return dp[0][0][2];
+   
+}
 };
