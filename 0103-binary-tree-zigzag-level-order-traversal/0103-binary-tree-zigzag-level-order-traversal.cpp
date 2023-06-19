f174b01e97c340a1862da932576d7808d7f5ee08
Aurobind sahu <62556848+badalsahu363@users.noreply.github.com>
2023-06-19 11:41:33 +0530

    Time: 10 ms (9.48%), Space: 12 MB (60.79%) - LeetHub

--- 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
+++ 0103-binary-tree-zigzag-level-order-traversal/0103-binary-tree-zigzag-level-order-traversal.cpp
@@ -1,42 +1,38 @@
 /**
- *Definition for a binary tree node.
- *struct TreeNode {
- *  int val;
- *  TreeNode * left;
- *  TreeNode * right;
- *  TreeNode() : val(0), left(nullptr), right(nullptr) {}
- *  TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
- *  TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
- *};
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
  */
-class Solution
-{
-    public:
-        vector<vector < int>> zigzagLevelOrder(TreeNode *root)
-        {
-            vector<vector < int>> ans;
-            if (root == NULL) return ans;
-            queue<TreeNode*> q;
-            q.push(root);
-            bool leftToRight=true;
-            while (!q.empty())
-            {
-                int size = q.size();
-                vector<int> level(size);
-                for (int i = 0; i < size; i++)
-                {
-                    TreeNode *front = q.front();
-                    q.pop();
-                    //find the position to insert
-                    int index=(leftToRight)?i:(size-1-i);
-                    if (front->left != NULL) q.push(front->left);
-                    if (front->right != NULL) q.push(front->right);
-                    level[index]=front->val;
-                }
-                leftToRight=!leftToRight;
-                ans.push_back(level);
+class Solution {
+public:
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
+         vector<vector<int>>ans;
+        queue<TreeNode*>q;
+        bool LeftToRight= true; // left -> right
+        if(root == NULL) return ans;
+        q.push(root);
+        while(!q.empty()){
+            
+            int n = q.size();
+            vector<int>level(n);
+            for(int i=0;i<n;i++){
+                TreeNode* front = q.front();
+                q.pop();
+                int index = (LeftToRight)?i:(n-1-i);
+                level[index] = front->val;
+                if(front->left != NULL) q.push(front->left);
+                if(front->right != NULL) q.push(front->right);
             }
-
-            return ans;
+             
+              LeftToRight = !LeftToRight;
+               ans.push_back(level);
         }
+        return ans;
+    }
 };
