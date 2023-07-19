5f97629a553829fd9e6cf4adf040ed55d1e9a5f6
Aurobind sahu <62556848+badalsahu363@users.noreply.github.com>
2023-07-19 10:20:14 +0530

    Time: 195 ms (54.29%), Space: 98.6 MB (33.36%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, int x) {
-        int sum = accumulate(nums.begin(), nums.end(), 0);
-        int reqSum = sum - x;
-        if (reqSum < 0) {
-            return -1;
-        }
-        if (reqSum == 0) {
-            return nums.size();
-        }
+         int sum = accumulate(nums.begin(),nums.end(),0);
+         int reqSum = sum-x;
+        if(reqSum == 0) return nums.size();
+           if(reqSum <0) return -1;
+        int tail=0,head=0,mxlen=0,mysum=0;
         
-        int tail = 0, mxlen = -1, mysum = 0;
-        for (int head = 0; head < nums.size(); ++head) {
+        
+        
+        for(head =0; head < nums.size();head++){
             mysum += nums[head];
-            while (mysum > reqSum) {
+            while(mysum > reqSum){
                 mysum -= nums[tail];
                 tail++;
             }
-            if (mysum == reqSum) {
-                mxlen = max(mxlen, head - tail + 1);
+            if(mysum == reqSum) {
+                mxlen = max(mxlen, head-tail+1);
             }
         }
-        
-        return (mxlen == -1) ? -1 : (nums.size() - mxlen);
+        return (mxlen==0)?-1:(nums.size()-mxlen);
     }
 };
