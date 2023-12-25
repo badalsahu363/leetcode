e17795d6445211ba2f6aa550cbb91009d53f7dbb
Aurobind sahu <62556848+badalsahu363@users.noreply.github.com>
2023-12-25 15:16:40 +0530

    Time: 207 ms (65.98%), Space: 99.9 MB (69.68%) - LeetHub

--- 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     int maximumUniqueSubarray(vector<int>& nums) {
-        int head,tail=0,sum =0,ans = 0;
-        unordered_map<int,int>mpp; // element and frequency
-        for(head =0;head<nums.size();head++){
+        int head,tail=0,sum=0,ans=0;
+        unordered_map<int,int>mpp;
+        for(head = 0;head < nums.size();head++){
             mpp[nums[head]]++;
             while(mpp[nums[head]] > 1){
                 ans = max(ans,sum);
@@ -11,7 +11,6 @@ public:
                 mpp[nums[tail]]--;
                 tail++;
             }
-            //operation performed
             if(mpp[nums[head]] == 1){
                 sum += nums[head];
             }
